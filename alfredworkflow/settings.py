f7bce7cabea05b8988e106a44a758d2e07e8138f
Tom Gorup <trynreadme@live.com>
2022-01-12 13:58:31 -0600

    Updating HOSTNAME subdomain
    Found that I was running into authentication issues when attempting to run commands. This resolved my issue.

--- alfredworkflow/settings.py
+++ alfredworkflow/settings.py
@@ -1,5 +1,5 @@
 PROTOCOL = "https"
-HOSTNAME = "graph.api.smartthings.com"
+HOSTNAME = "graph-na04-useast2.api.smartthings.com"
 PORT = 443
 SERVER_PORT = 2222
 CLIENT_ID = 'cb87b758-af81-11e2-981c-9989e5deaab7'
